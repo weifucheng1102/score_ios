0dc10589f78bd2fafdc44c39e2264092da350f9e
weifucheng <39686870+weifucheng1102@users.noreply.github.com>
2020-01-09 13:35:30 +0800

    Update score_ios.dart

--- lib/score_ios.dart
+++ lib/score_ios.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 class ScoreIos {
   static const MethodChannel _channel = const MethodChannel('score_ios');
-  static Future showAppStoreReView(String appStoreUrl) async {
-    await _channel.invokeMethod('showReview', [appStoreUrl]);
+  static Future showAppStoreReView(String appid) async {
+    await _channel.invokeMethod('showReview', [appid]);
   }
 }
